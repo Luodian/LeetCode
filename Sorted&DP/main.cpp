53ecdf2e162b46340e9c51bf4a4fa693cb4c3af9
Luodian <drluodian@gmail.com>
2019-04-13 16:40:35 +0800

    Update main.cpp
    Question again, when can I be admitted into Google?

--- Sorted&DP/main.cpp
+++ Sorted&DP/main.cpp
@@ -72,6 +72,56 @@ class Solution
 		}
 		return ans;
     }
+	int maxProfit(vector<int>& prices)
+	{    
+		int min_value = 0x3f3f3f3;
+		int max_value = 0;
+		int ans = 0;
+		for (int i = 0; i < prices.size(); ++i)
+		{
+			if (prices[i] < min_value)
+			{
+				min_value = prices[i];
+			}
+			if (prices[i] - min_value > max_value)
+			{
+				max_value = prices[i] - min_value;
+			}
+		}
+		return max_value;
+    }
+	int maxSubArray(vector<int>& nums)
+	{
+		int fn = 0;
+		int ans = 0x3f3f3f3f;
+		for (int i = 0; i < nums.size(); ++i)
+		{
+			fn = max(nums[i], fn + nums[i]);
+			ans = max(ans,fn);
+		}
+		return ans;
+    }
+	int findShortestSubArray(vector<int>& nums) {
+        map<int, vector<int>> mp;
+		for (int i = 0;i < nums.size(); ++i)
+		{
+			mp[nums[i]].push_back(i);
+		}
+		int max_len = 0;
+		for (auto &item : mp)
+		{
+			max_len = max(max_len, item.second.size());	
+		}
+		int min_ans = 0x3f3f3f3f;
+		for (auto &item : mp)
+		{
+			if (item.second.size() == max_len)
+			{
+				min_ans = min(min_ans, item.second[-1] - item.second[0]);
+			}
+		}
+		return min_ans;
+    }
 };
 
 int main ()
